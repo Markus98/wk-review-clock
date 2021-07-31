c4b8a7a76fa9ee21de033836453f3245289fd752
Markus Tuominen <markus@tuominen.net>
2021-07-31 18:20:07 +0300

    First working version

--- wk-review-clock.js
+++ wk-review-clock.js
@@ -10,22 +10,42 @@
 // @license     GPL version 3 or later: http://www.gnu.org/copyleft/gpl.html
 // ==/UserScript==
 
-const defaultSettingsObj = {
-    showTimer: true,
-    showRate: true,
-    showTimeRemaining: true
-};
 let statsDivObj;
+let settings;
+let time;
+
+const timerSettingsKey = 'reviewTimerSettings';
+const timerTimeKey = 'reviewTimerTime';
+
+function setSettings(showTimer, showRate, showTimeRemaining) {
+    const settingsObj = {
+        showTimer,
+        showRate,
+        showTimeRemaining
+    };
+    window.localStorage.setItem(timerSettingsKey, JSON.stringify(settingsObj));
+    settings = settingsObj;
+}
+
+function getSettings() {
+    const settingsStr = window.localStorage.getItem(timerSettingsKey);
+    return settingsStr ? JSON.parse(settingsStr) : null;
+}
 
 function incrementTimer(t) {
-    const previous = window.localStorage.reviewTimerTime;
-    window.localStorage.reviewTimerTime = previous + t;
+    time += t;
+    window.localStorage.setItem(timerTimeKey, time);
+}
+
+function setTimer(t) {
+    time = t;
+    window.localStorage.setItem(timerTimeKey, t);
 }
 
 function splitToHourMinSec(timeSec) {
-    const h = Math.floor(timeSec / 60 / 60);
+    const h = Math.floor( timeSec/60/60 );
     const min = Math.floor( (timeSec - (h*60*60)) / 60 );
-    const sec = timeSec - h*60*60 - min*60;
+    const sec = Math.round( timeSec - h*60*60 - min*60 );
     return {h, min, sec};
 }
 
@@ -35,11 +55,9 @@ function getTimeString(hourMinSec) {
 }
 
 function getCurrentTimerString() {
-    const settings = window.localStorage.reviewTimerSettings;
-    const time = window.localStorage.reviewTimerTime;
     const hourMinSec = splitToHourMinSec(time);
 
-    let timerString = 'asd';
+    let timerString = '';
     if (settings.showTimer) {
         timerString += 'Elapsed: ' + getTimeString(hourMinSec) + '\n';
     }
@@ -77,19 +95,18 @@ function startTimer (intervalSec) {
 }
 
 function startReviewTimer() {
-    statsDivObj = generateStatsDiv();
-
     // append statsDiv to header
     const header = document.getElementById('summary-button');
     header.appendChild(statsDivObj.statDiv);
 
     // Init default settings
-    if (!window.localStorage.reviewTimerSettings) {
-        window.localStorage.reviewTimerSettings = defaultSettingsObj;
+    if (!settings) {
+        setSettings(true, true, true);
     }
-    // init timer to 0s
-    window.localStorage.reviewTimerTime = 0;
+    // Init timer to 0s
+    setTimer(0);
 
+    // Start the timer
     startTimer(1.0);
 }
 
@@ -98,6 +115,8 @@ function showLastReviewStats() {
 }
 
 function main() {
+    settings = getSettings();
+    statsDivObj = generateStatsDiv();
     if(/session$/.exec(window.location.href)) { // review page
         startReviewTimer();
     } else { // review summary page
