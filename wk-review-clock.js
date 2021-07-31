fe3ea9733c573f09c4a48d9233138cb13038a4d9
Markus Tuominen <markus@tuominen.net>
2021-07-31 22:26:21 +0300

    Added WK Open Framework settings

--- wk-review-clock.js
+++ wk-review-clock.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        WaniKani ReviewClock
+// @name        WaniKani Review Clock
 // @namespace   wkreviewclock
 // @description 
 // @include     http://www.wanikani.com/review*
@@ -11,26 +11,11 @@
 // ==/UserScript==
 
 let statHtmlElems;
-let settings;
 let time;
+let wkOpenFrameWorkLoaded = false;
 
-const timerSettingsKey = 'reviewTimerSettings';
 const timerTimeKey = 'reviewTimerTime';
-
-function setSettings(showTimer, showRate, showTimeRemaining) {
-    const settingsObj = {
-        showTimer,
-        showRate,
-        showTimeRemaining
-    };
-    window.localStorage.setItem(timerSettingsKey, JSON.stringify(settingsObj));
-    settings = settingsObj;
-}
-
-function getSettings() {
-    const settingsStr = window.localStorage.getItem(timerSettingsKey);
-    return settingsStr ? JSON.parse(settingsStr) : null;
-}
+const scriptId = 'WKReviewClock'
 
 function incrementTimer(t) {
     time += t;
@@ -57,18 +42,32 @@ function getTimeString(hourMinSec) {
 function setCurrentTimerStats() {
     const hourMinSec = splitToHourMinSec(time);
 
+    const settings = {
+        showTimer: true,
+        showRate: true,
+        showRemaining: true
+    }
+
+    if (wkOpenFrameWorkLoaded) {
+        settings.showTimer = wkof.settings[scriptId].showTimer;
+        settings.showRate = wkof.settings[scriptId].showRate;
+        settings.showTimeRemaining = wkof.settings[scriptId].showRemaining;
+    }
+
     if (settings.showTimer) {
         statHtmlElems.timer.span.textContent =  getTimeString(hourMinSec);
     }
+
     const reviewsDoneNumber = parseInt(document.getElementById('completed-count').textContent);
     const reviewRate = reviewsDoneNumber/time; // reviews/sec
     if (settings.showRate) {
-        const formattedRate = (reviewRate*60).toFixed(1); // reviews/min
-        statHtmlElems.rate.span.textContent = formattedRate + ' r/min';
+        const formattedRate = (reviewRate*3600).toFixed(1); // reviews/hour
+        statHtmlElems.rate.span.textContent = formattedRate + ' r/h';
     }
+
     const reviewsAvailableNumber = parseInt(document.getElementById('available-count').textContent);
     const timeRemaining = reviewsAvailableNumber / reviewRate; // seconds
-    if (settings.showTimeRemaining) {
+    if (settings.showRemaining) {
         let remainingStr = 'Est. ';
         if (Number.isFinite(timeRemaining)) {
             remainingStr += getTimeString(splitToHourMinSec(timeRemaining));
@@ -112,10 +111,20 @@ function generateStatHtmlElems() {
             span: remainingSpan
         },
         updateVisibility: function() {
-            this.timer.timerIcon.style.cssText = settings.showTimer ? "" : "display: none;";
-            this.timer.timerSpan.style.cssText = settings.showTimer ? "" : "display: none;";
+            if (!wkOpenFrameWorkLoaded) return;
+            const settings = wkof.settings[scriptId];
+            if (settings) {
+                const disp = (bool) => bool ? "" : "display: none;";
+                this.timer.icon.style.cssText = disp(settings.showTimer);
+                this.timer.span.style.cssText = disp(settings.showTimer);
+                this.rate.icon.style.cssText = disp(settings.showRate);
+                this.rate.span.style.cssText = disp(settings.showRate);
+                this.remaining.icon.style.cssText = disp(settings.showRemaining);
+                this.remaining.span.style.cssText = disp(settings.showRemaining);
+            }
         }
     }
+    statHtmlElems.updateVisibility();
 }
 
 const setStatsDivContentAndIncrementTimer = (intervalSec) => () => {
@@ -137,10 +146,6 @@ function startReviewTimer() {
     header.appendChild(statHtmlElems.remaining.icon);
     header.appendChild(statHtmlElems.remaining.span);
 
-    // Init default settings
-    if (!settings) {
-        setSettings(true, true, true);
-    }
     // Init timer to 0s
     setTimer(0);
 
@@ -152,14 +157,70 @@ function showLastReviewStats() {
 
 }
 
-function main() {
-    settings = getSettings();
-    generateStatHtmlElems();
+function openSettings() {
+    var config = {
+        script_id: scriptId,
+        title: 'Review Clock Settings',
+        on_save: () => {
+            wkof.Settings.save(scriptId);
+            statHtmlElems.updateVisibility();
+        },
+        content: {
+            showTimer: {
+                type: 'checkbox',
+                label: 'Show timer',
+                default: true,
+                hover_tip: 'Asd.',
+            },
+            showRate: {
+                type: 'checkbox',
+                label: 'Show review speed',
+                default: true,
+                hover_tip: 'Asd.',
+            },
+            showRemaining: {
+                type: 'checkbox',
+                label: 'Show remaining timer',
+                default: true,
+                hover_tip: 'Asd.',
+            }
+        }
+    }
+    var dialog = new wkof.Settings(config);
+    dialog.open();
+}
+
+function installSettingsMenu() {
+    wkof.Menu.insert_script_link({
+        name:      'review_clock_settings',
+        submenu:   'Settings',
+        title:     'Review Clock',
+        on_click:  openSettings
+    });
+}
+
+async function main() {
+    try {
+        const wkofModules = 'Settings,Menu';
+        wkof.include(wkofModules);
+        wkOpenFrameWorkLoaded = true;
+        await wkof.ready(wkofModules)
+            .then(() => wkof.Settings.load(scriptId))
+            .then(installSettingsMenu);
+    } catch (err) {
+        if (err instanceof ReferenceError) {
+            console.warn("Wanikani Open FrameWork required for adjusting WaniKani Review Clock settings.");
+        } else {
+            throw err;
+        }
+    }
+    
     if(/session$/.exec(window.location.href)) { // review page
+        await generateStatHtmlElems();
         startReviewTimer();
     } else { // review summary page
         showLastReviewStats();
     }
 }
 
-window.onload = main();
+main();
